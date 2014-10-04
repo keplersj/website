57a424bcddeb95eab79a0f628edeb635118fa82f
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-10-04 17:35:48 -0600

    Use native window.

--- source/js/plugins.rb
+++ source/js/plugins.rb
@@ -1,7 +1,7 @@
 require 'opal'
 
 # Avoid `console` errors in browsers that lack a console.
-unless ($window.console && console.log)
+unless (Native(`window`).console && console.log)
   methods = [:assert, :clear, :count, :debug, :dir, :dirxml, :error, :exception, :group, :groupCollapsed, :groupEnd, :info, :log, :markTimeline, :profile, :profileEnd, :markTimeline, :table, :time, :timeEnd, :timeStamp, :trace, :warn]
   
   methods.each do |method|
