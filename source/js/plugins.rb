d8b6f684a7be32f11ad20113e1ce9e3ed3cb085a
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-10-04 17:37:09 -0600

    Clean up Array.
    What fun is using Ruby is you don't have a clean script.

--- source/js/plugins.rb
+++ source/js/plugins.rb
@@ -2,7 +2,31 @@ require 'opal'
 
 # Avoid `console` errors in browsers that lack a console.
 unless (Native(`window`).console && console.log)
-  methods = [:assert, :clear, :count, :debug, :dir, :dirxml, :error, :exception, :group, :groupCollapsed, :groupEnd, :info, :log, :markTimeline, :profile, :profileEnd, :markTimeline, :table, :time, :timeEnd, :timeStamp, :trace, :warn]
+  methods = [
+    :assert,
+    :clear,
+    :count,
+    :debug,
+    :dir,
+    :dirxml,
+    :error,
+    :exception,
+    :group,
+    :groupCollapsed,
+    :groupEnd,
+    :info,
+    :log,
+    :markTimeline,
+    :profile,
+    :profileEnd,
+    :markTimeline,
+    :table,
+    :time,
+    :timeEnd,
+    :timeStamp,
+    :trace,
+    :warn
+  ]
   
   methods.each do |method|
     remove_method method
