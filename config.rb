951d6c8f7a631834f94e8e5c1a2da706ab35257a
Kepler Sticka-Jones <kbsj269@gmail.com>
2014-10-04 18:58:13 -0600

    Update config.rb
    [ci skip]

--- config.rb
+++ config.rb
@@ -29,7 +29,7 @@ set :slim, :layout_engine => :slim
 #   page "/admin/*"
 # end
 
-page "crossdomain.xml", :layout => :slim
+page "crossdomain.xml.slim", :layout => :slim
 
 # Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
