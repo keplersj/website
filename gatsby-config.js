fe7bc3738195db5a062ad5f2fa7810250ad80be6
Kepler Sticka-Jones <kepler+git@stickajones.org>
2018-07-31 19:09:44 -0600

    Add Gatsby WebManifest plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,6 +2,18 @@ module.exports = {
   plugins: [
     "gatsby-plugin-react-helmet",
     "gatsby-plugin-typescript",
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Kepler Sticka-Jones",
+        short_name: "Kepler Sticka-Jones",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#409040",
+        display: "minimal-ui",
+        icon: "src/images/avatar.jpeg"
+      }
+    },
     "gatsby-plugin-offline"
   ]
 };
