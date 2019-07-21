bf916c879a465ac69fa568dba2c1490e2e423ccd
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-21 15:08:55 -0600

    Create basic unit test for project page

---
+++ src/templates/project-page.test.tsx
@@ -0,0 +1,64 @@
+import * as React from "react";
+import renderer from "react-test-renderer";
+import { StaticQuery } from "gatsby";
+import Page from "./project-page";
+
+beforeEach(() => {
+  (StaticQuery as jest.Mock).mockImplementation(({ children }) =>
+    children({
+      file: {
+        childImageSharp: {
+          fixed: {
+            base64: ""
+          }
+        }
+      },
+      backdrop: {
+        childImageSharp: {
+          fluid: {}
+        }
+      },
+      site: {
+        siteMetadata: {
+          title: "",
+          description: "",
+          siteUrl: "",
+          twitterUsername: "",
+          instagramUsername: "",
+          linkedinUsername: "",
+          githubUsername: ""
+        }
+      }
+    })
+  );
+});
+
+describe("Project Page Template", () => {
+  it("renders correctly", () => {
+    const tree = renderer
+      .create(
+        <Page
+          data={{
+            markdownRemark: {
+              excerpt: "",
+              html: "",
+              fields: {
+                slug: ""
+              },
+              frontmatter: {
+                title: "",
+                date: "",
+                description: ""
+              },
+              timeToRead: "",
+              wordCount: {
+                words: ""
+              }
+            }
+          }}
+        />
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
