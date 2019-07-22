b0c527737d6c7a38137ce0c9b54409bc57dde7ac
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-22 13:33:42 -0600

    Add jest-runner-eslint

--- jest.config.js
+++ jest.config.js
@@ -1,23 +1,33 @@
 /* eslint-disable */
 module.exports = {
-  transform: {
-    "^.+\\.tsx?$": "ts-jest",
-    "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js"
-  },
-  testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
-  moduleNameMapper: {
-    ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
-    ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-      "<rootDir>/__mocks__/file-mock.js"
-  },
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  testPathIgnorePatterns: ["node_modules", ".cache"],
-  transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
-  globals: {
-    __PATH_PREFIX__: ""
-  },
-  testURL: "http://localhost",
-  setupFiles: ["<rootDir>/loadershim.js"],
-  snapshotSerializers: ["jest-emotion"],
-  collectCoverage: true
+  projects: [
+    {
+      displayName: "test",
+      transform: {
+        "^.+\\.tsx?$": "ts-jest",
+        "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js"
+      },
+      testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
+      moduleNameMapper: {
+        ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
+        ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
+          "<rootDir>/__mocks__/file-mock.js"
+      },
+      moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+      testPathIgnorePatterns: ["node_modules", ".cache"],
+      transformIgnorePatterns: ["node_modules/(?!(gatsby)/)"],
+      globals: {
+        __PATH_PREFIX__: ""
+      },
+      testURL: "http://localhost",
+      setupFiles: ["<rootDir>/loadershim.js"],
+      snapshotSerializers: ["jest-emotion"],
+      collectCoverage: true
+    },
+    {
+      runner: "jest-runner-eslint",
+      displayName: "lint:eslint",
+      testMatch: ["<rootDir>/src/**/*.tsx", "<rootDir>/src/**/*.ts"]
+    }
+  ]
 };
--- src/__mocks__/gatsby.ts
+++ src/__mocks__/gatsby.ts
@@ -1,4 +1,4 @@
-const React = require("react");
+import React = require("react");
 const gatsby = jest.requireActual("gatsby");
 
 module.exports = {
