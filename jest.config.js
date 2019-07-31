30492e59390ab14118a6acb868faff45e396cb0e
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-31 12:29:10 -0600

    Move custom Jest setup scripts to new directory

--- jest.config.js
+++ jest.config.js
@@ -6,13 +6,13 @@ module.exports = {
       displayName: "test",
       transform: {
         "^.+\\.tsx?$": "ts-jest",
-        "^.+\\.jsx?$": "<rootDir>/jest-preprocess.js"
+        "^.+\\.jsx?$": "<rootDir>/.jest/gatsby-preprocess.js"
       },
       testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.([tj]sx?)$",
       moduleNameMapper: {
         ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
         ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-          "<rootDir>/__mocks__/file-mock.js"
+          "<rootDir>/src/__mocks__/file-mock.js"
       },
       moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
       testPathIgnorePatterns: ["node_modules", ".cache"],
@@ -21,7 +21,7 @@ module.exports = {
         __PATH_PREFIX__: ""
       },
       testURL: "http://localhost",
-      setupFiles: ["<rootDir>/loadershim.js"],
+      setupFiles: ["<rootDir>/.jest/loadershim.js"],
       snapshotSerializers: ["jest-emotion"],
       collectCoverage: true,
       coverageReporters: ["json", "text"]
