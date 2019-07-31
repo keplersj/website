90433aa0d68aca6649ef4148941a27a26e234b2a
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-31 12:30:42 -0600

    Use jest-raw-loader instead of custom file mock

--- jest.config.js
+++ jest.config.js
@@ -12,7 +12,7 @@ module.exports = {
       moduleNameMapper: {
         ".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
         ".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$":
-          "<rootDir>/src/__mocks__/file-mock.js"
+          "jest-raw-loader"
       },
       moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
       testPathIgnorePatterns: ["node_modules", ".cache"],
