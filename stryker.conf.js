62a92a7b2896ad54b7e7da836068e06304dbc6ea
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-07-31 14:25:24 -0600

    Update stryker not to mutate stories

--- stryker.conf.js
+++ stryker.conf.js
@@ -7,6 +7,7 @@ module.exports = function(config) {
     mutate: [
       "src/**/*.ts?(x)",
       "!src/**/*@(.test|.spec|Spec).ts?(x)",
+      "!src/**/*@(.stories).ts?(x)",
       "!src/__mocks__/**/*.ts?(x)"
     ],
     mutator: "typescript",
