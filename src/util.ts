ecb51e88cec00f6a7915fac66fdf6bd156d77f7d
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-09-29 00:18:28 -0600

    Remove unused utility function

--- src/util.ts
+++
@@ -1,9 +0,0 @@
-export function getDescription(generated?: string, provided?: string): string {
-  if (provided) {
-    return provided;
-  } else if (generated) {
-    return generated;
-  } else {
-    return "";
-  }
-}
