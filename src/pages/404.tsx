509037b4df5dcfbee18452408491549138e4d890
Kepler Sticka-Jones <kepler+git@stickajones.org>
2019-11-30 14:57:21 -0700

    Refactor and simplify 404 a bit

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,10 +1,7 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { Global, css } from "@emotion/core";
 import styled from "@emotion/styled";
 import BaseLayout from "../components/BaseLayout";
-import { Helmet } from "react-helmet";
-import { WebPage } from "schema-dts";
 import { BannerBackground } from "../components/BannerBackground";
 
 const Background = styled(BannerBackground)`
@@ -13,6 +10,10 @@ const Background = styled(BannerBackground)`
 `;
 
 const Container = styled.div`
+  color: #505050;
+  font-size: 1.5em;
+  font-weight: 500;
+  line-height: 1.5;
   height: 100vh;
   width: 100vw;
   display: flex;
@@ -27,41 +28,18 @@ const Container = styled.div`
 `;
 
 const FourOhFourPage = (): React.ReactElement => (
-  <>
-    <Helmet>
-      <script type="application/ld+json">
-        {JSON.stringify({
-          "@context": "http://www.schema.org",
-          "@type": "WebPage",
-          name: "404! | Kepler Sticka-Jones"
-        } as WebPage)}
-      </script>
-    </Helmet>
-
-    <BaseLayout title="404!" hideNavbar>
-      <Global
-        styles={css`
-          body {
-            color: #505050;
-            font-size: 1.5em;
-            font-weight: 500;
-            line-height: 1.5;
-          }
-        `}
-      />
-
-      <Background highQuality Tag="section">
-        <Container>
-          <div>
-            <h1>404!</h1>
-            <span>
-              Maybe, go back to the <Link to="/">home page</Link>?
-            </span>
-          </div>
-        </Container>
-      </Background>
-    </BaseLayout>
-  </>
+  <BaseLayout title="404!" hideNavbar>
+    <Background highQuality Tag="section">
+      <Container>
+        <div>
+          <h1>404!</h1>
+          <span>
+            Maybe, go back to the <Link to="/">home page</Link>?
+          </span>
+        </div>
+      </Container>
+    </Background>
+  </BaseLayout>
 );
 
 export default FourOhFourPage;
