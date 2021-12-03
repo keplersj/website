3b8869b60a7b1439cdf64918ad2e358866a272eb
Kepler Sticka-Jones <kepler@stickajones.org>
2021-12-02 23:46:17 -0700

    Beginning Markdown rendering parity using Unified plugins

---
+++ src/components/Markdown.js
@@ -0,0 +1,31 @@
+import { c, html, useRef } from "atomico";
+import { useSlot } from "@atomico/hooks/use-slot";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import rehypeSlug from "rehype-slug";
+import "webcomponent-markdown";
+
+// This is so hacky and I hate it, but I don't want to have to configure unified plugins more than once
+
+function component() {
+  const inlineContentRef = useRef();
+  const inlineContentChildNodes = useSlot(inlineContentRef);
+
+  return html`
+    <host shadowDom>
+      <slot name="content" ref="${inlineContentRef}">
+        <script type="text/markdown"></script>
+      </slot>
+      <remark-markdown
+        remarkPlugins=${[remarkGfm, remarkFrontmatter]}
+        rehypePlugins=${[rehypeSlug]}
+      >
+        <script slot="content">
+          ${inlineContentChildNodes[0]?.textContent || ""};
+        </script>
+      </remark-markdown>
+    </host>
+  `;
+}
+
+customElements.define("kepler-markdown", c(component));
