5602265f75e454c22c3aa14b3003055ab7aed812
Kepler Sticka-Jones <kepler@stickajones.org>
2021-12-12 23:23:26 -0700

    Blog posts rendering post title, date published, shifting child headings

---
+++ src/components/MarkdownPost.js
@@ -0,0 +1,77 @@
+import { c, html, useRef, useState, useCallback } from "atomico";
+import { useSlot } from "@atomico/hooks/use-slot";
+import remarkFrontmatter from "remark-frontmatter";
+import remarkGfm from "remark-gfm";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeSlug from "rehype-slug";
+import rehypeShiftHeading from "rehype-shift-heading";
+import yaml from "js-yaml";
+import "webcomponent-markdown";
+
+// This is so hacky and I hate it, but I don't want to have to configure unified plugins more than once
+
+function component() {
+  const [title, setTitle] = useState("");
+  const [datePublished, setDatePublished] = useState("");
+  const inlineContentRef = useRef();
+  const inlineContentChildNodes = useSlot(inlineContentRef);
+
+  const frontMatterExtract = useCallback(
+    () => (tree) => {
+      const yamlNode = tree.children.find((child) => child.type === "yaml");
+      if (yamlNode) {
+        // console.dir(yamlNode);
+        const parsed = yaml.load(yamlNode.value);
+        // console.log(parsed);
+        // setMetadata({
+        //   title: parsed.title,
+        //   datePublished: parsed.date,
+        // });
+        setTitle(parsed.title);
+        setDatePublished(String(parsed.date));
+        return;
+      } else {
+        // console.log("No YAML node found");
+      }
+    },
+    [setDatePublished, setTitle]
+  );
+
+  return html`
+    <host shadowDom>
+      <slot name="content" ref="${inlineContentRef}">
+        <script type="text/markdown"></script>
+      </slot>
+      <header>
+        <h1>${title}</h1>
+        <div>
+          <span>
+            Published${" "}
+            <time datetime=${datePublished}>
+              ${new Date(datePublished).toLocaleDateString("en-US", {
+                month: "long",
+                day: "numeric",
+                year: "numeric",
+              })}
+            </time>
+          </span>
+        </div>
+      </header>
+      <remark-markdown
+        remarkPlugins=${[remarkGfm, remarkFrontmatter, frontMatterExtract]}
+        rehypePlugins=${[
+          rehypeSlug,
+          rehypeAutolinkHeadings,
+          [rehypeShiftHeading, { shift: 1 }],
+        ]}
+      >
+        <!-- prettier-ignore -->
+        <script slot="content">
+          ${inlineContentChildNodes[0]?.textContent || ""}
+        </script>
+      </remark-markdown>
+    </host>
+  `;
+}
+
+customElements.define("kepler-markdown-post", c(component));
