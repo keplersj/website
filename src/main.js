bf87039b2b86e6eaa7adb5b5822344bf210568f1
Kepler Sticka-Jones <kepler@stickajones.org>
2021-12-24 02:50:59 -0700

    Atomico SSR optimizations
    https://atomico.gitbook.io/doc/api/testing/test-dom#ssr

--- src/main.js
+++ src/main.js
@@ -1,9 +1,14 @@
 import "./main.css";
 import { sheet } from "@emotion/css";
+import { options } from "atomico";
 
 // Check if the root node has any children to detect if the app has been preprendered
 // speedy is disabled when the app is being prerendered so that styles render into the DOM
 // speedy is significantly faster though so it should only be disabled during prerendering
 if (document.querySelector("html[data-prerender]")) {
   sheet.speedy(false);
+
+  options.sheet = false;
+
+  options.ssr = true;
 }
