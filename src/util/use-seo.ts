06f554e9154d8121ffc14e73ad1462e71f4302d3
Kepler Sticka-Jones <kepler@stickajones.org>
2022-02-16 18:34:14 -0700

    Very basic base SEO

--- src/util/use-seo.ts
+++ src/util/use-seo.ts
@@ -1,11 +1,18 @@
 import { useHead } from "atomico-use-head";
 
+const siteUrlBase = "https://keplersj.com";
+
 interface Metadata {
   title: string;
+  description: string;
+  path: string;
 }
 
 const defaultMetadata: Metadata = {
   title: "Kepler Sticka-Jones",
+  description:
+    "Kepler Sticka-Jones is a computer programmer and college student based out of Salt Lake City, with experience in entrepreneurship, student leadership, and open source software development.",
+  path: "/",
 };
 
 export function useSEO(metadata: Partial<Metadata> = defaultMetadata) {
@@ -23,6 +30,38 @@ export function useSEO(metadata: Partial<Metadata> = defaultMetadata) {
         metadata.title && metadata.title !== defaultMetadata.title
           ? `${metadata.title} | ${defaultMetadata.title}`
           : defaultMetadata.title,
+      meta: [
+        {
+          name: "description",
+          content: metadataComposite.description,
+        },
+        {
+          name: "og:site_name",
+          content: defaultMetadata.title,
+        },
+        {
+          name: "og:title",
+          content: metadataComposite.title,
+        },
+        {
+          name: "og:url",
+          content: `${siteUrlBase}${metadataComposite.path}`,
+        },
+        {
+          name: "og:description",
+          content: metadataComposite.description,
+        },
+        {
+          name: "twitter:card",
+          content: "summary",
+        },
+      ],
+      link: [
+        {
+          rel: "canonical",
+          href: `${siteUrlBase}${metadataComposite.path}`,
+        },
+      ],
     },
     {
       hydrate: true,
